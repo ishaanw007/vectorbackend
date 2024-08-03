8ebd7437b05cafe4b100ebe9303e9d5f515a783f
ishaan walecha <ishaanwalecha@ishaans-Laptop.local>
2024-08-03 16:09:04 +0530

    i

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to match your frontend URL
+    allow_origins=["*"],  # Adjust this to match your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
